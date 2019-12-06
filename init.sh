01b5d6655645001ce8f54bfd211dfdab3d4651b3
Imran-S-heikh <52422304+Imran-S-heikh@users.noreply.github.com>
2019-12-06 11:20:34 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 npm init
 npm install -D babel-loader @babel/core @babel/preset-env webpack webpack-cli css-loader html-loader html-webpack-plugin mini-css-extract-plugin webpack-dev-server sass-loader node-sass file-loader autoprefixer postcss-loader
 gnome-terminal -- npm start
-rm file_1.txt
+rm init.sh
