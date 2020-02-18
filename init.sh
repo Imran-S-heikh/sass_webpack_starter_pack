8411266e77f3a20e7a52e24e5bbfa4f1c0841429
Imran-S-heikh <52422304+Imran-S-heikh@users.noreply.github.com>
2020-02-18 09:21:07 +0600

    git init added

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git init
+touch .gitignore
+echo 'node_modules/' >> .gitignore
 npm init
 npm install -D babel-loader @babel/core @babel/preset-env webpack webpack-cli css-loader html-loader html-webpack-plugin mini-css-extract-plugin webpack-dev-server sass-loader node-sass file-loader autoprefixer postcss-loader
 gnome-terminal -- npm start
