59f42779f451dfd81e92a66815e76b19309dcbca
Imran-S-heikh <52422304+Imran-S-heikh@users.noreply.github.com>
2020-03-06 07:11:08 +0600

    html image error updated

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,8 @@ module.exports = {
         test: /\.(png|jpeg|gif|jpg|svg)$/i,
         loader: 'file-loader',
         options: {
-          name: './img/[name].[ext]',
+          name: '[path][name].[ext]',
+          context: path.resolve(__dirname, "src/")
         },
       },
       {
@@ -67,6 +68,7 @@ module.exports = {
         use: [{
           loader: 'html-loader',
           options: {
+            interpolate: true,
             minimize: false,
             removeComments: false,
             collapseWhitespace: false
