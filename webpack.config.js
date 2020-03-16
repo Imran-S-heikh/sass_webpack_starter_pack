ba1f18ca842ff08769bc1224122c6c3eb440f5be
Imran-S-heikh <52422304+Imran-S-heikh@users.noreply.github.com>
2020-03-16 08:34:39 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // all options are optional
-      filename: './css/style.css',
+      filename: 'style.css',
       chunkFilename: '[id].css',
       ignoreOrder: false, // Enable to remove warnings about conflicting order
     }),
