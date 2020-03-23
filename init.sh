612eeaab7658561bc56af8205a279abeb6c9e73c
Imran-S-heikh <52422304+Imran-S-heikh@users.noreply.github.com>
2020-03-23 19:51:27 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ echo 'node_modules/' >> .gitignore
 npm init
 sed -i 's/"test.*/"start": "webpack-dev-server","build": "webpack"/g' package.json
 npm install -D babel-loader @babel/core @babel/preset-env webpack webpack-cli css-loader html-loader html-webpack-plugin mini-css-extract-plugin webpack-dev-server sass-loader node-sass file-loader autoprefixer postcss-loader
-gnome-terminal -- npm start
+#gnome-terminal -- npm start
 rm init.sh
