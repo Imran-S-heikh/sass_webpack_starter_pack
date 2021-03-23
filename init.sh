d11abbdaf67c46ed3a8f9eb8a2fa52f1adbc7b7e
Imran Sheikh <imransheikhsadi@gmail.com>
2021-03-23 11:03:18 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ echo 'node_modules/' >> .gitignore
 npm init
 node js.script.js
 # sed -i 's/"test.*/"start": "webpack serve","build": "webpack"/g,"generate_icon": "icon-font-generator ./src/assets/icons/svg/*.svg -o ./src/assets/icons"' package.json
-npm install -D babel-loader @babel/core @babel/preset-env webpack-atomizer-loader webpack-molecule-loader webpack webpack-cli css-loader html-loader html-webpack-plugin mini-css-extract-plugin webpack-dev-server sass-loader node-sass file-loader autoprefixer postcss-loader icon-font-generator
+npm install -D babel-loader @babel/core @babel/preset-env webpack-atomizer-loader webpack-html-component-loader webpack webpack-cli css-loader html-loader html-webpack-plugin mini-css-extract-plugin webpack-dev-server sass-loader node-sass file-loader autoprefixer postcss-loader icon-font-generator
 #gnome-terminal -- npm start
 rm js.script.js
 rm init.sh
